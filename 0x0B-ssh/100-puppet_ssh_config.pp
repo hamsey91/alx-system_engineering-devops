860b703798b1c166df04c4ed067fbcfdeb1871e1
hamza <seyedi.hamza@gmail.com>
2024-03-24 19:47:18 -0700

    last commit

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Script that use puppet to change config.
+
+file { 	'ect/ssh/ssh_config':
+	 ensure => present,
+
+content =>"
+
+	#SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+
+}
