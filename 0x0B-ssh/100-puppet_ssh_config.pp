804449f20e37321ffcd6c9ea4c7b29ff10648a78
hamza <seyedi.hamza@gmail.com>
2024-03-26 06:55:39 -0700

    add

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
-# Script that use puppet to change config.
+# Script that use puppet to change config
 
-file {	'/ect/ssh/ssh_config':
-	ensure => present,
-
-content =>"
-
-	#SSH client configuration
-	Host*
+file { '/etc/ssh/ssh_config':
+	ensure  => present,
+content => "
+	# SSH client configuration
+	Host *
 	IdentityFile ~/.ssh/school
 	PasswordAuthentication no
-	",
-
+  	",
 }
